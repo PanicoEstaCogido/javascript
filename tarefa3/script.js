5ff28008405313f6173f2c577803307b98cd4e20
PanicoEstaCogido <56600651+PanicoEstaCogido@users.noreply.github.com>
2020-01-10 11:46:43 +0100

    Add files via upload

---
+++ tarefa3/script.js
@@ -0,0 +1,27 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: script.js
+*/
+
+/*Variables*/
+var uno;
+uno=1.1111;
+var dos;
+dos=2.2222;
+var tres;
+tres=3.3333;
+
+/*Valores nuevos*/
+var raíz=Math.sqrt(uno);
+var elevado=Math.pow(dos,2);
+var numeroPi=Math.PI;
+var numeroPi=Math.round(numeroPi);
+var menos=Math.min(uno,dos,tres);
+var aleatorio=Math.random(1,3);
+
+/*Resultados*/
+alert(raíz);
+alert(elevado);
+alert(numeroPi);
+alert(menos);
+alert(aleatorio);
