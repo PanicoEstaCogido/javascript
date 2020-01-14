2a89db980ef26d5c5b2099882f3e9822cf89c4d6
PanicoEstaCogido <56600651+PanicoEstaCogido@users.noreply.github.com>
2020-01-14 18:31:31 +0100

    Add files via upload

---
+++ tarefa5_ducias2ovos/script.js
@@ -0,0 +1,21 @@
+/* 
+	tarefa5_ducias2ovos
+	Nome do ficheiro: script.js
+	Usado: alert() prompt()
+*/
+
+var ducias = prompt("¿Cantas ducias fan falla para facer unha mega tortilla?");
+ducias = parseInt(ducias);
+var ovos = (ducias * 12 + " ovos")
+
+alert("dixeches "+ducias+" ducias")
+alert("polo tanto son "+ovos)
+
+/*
+var euros = prompt("¿Cantos €?");
+euros = parseInt(euros);
+var yenes = (euros * 130)
+
+alert("dixeches "+euros+" €")
+alert("polo tanto son "+yenes+" ¥")
+*/
